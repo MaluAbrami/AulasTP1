436cfa724ce3522afce5e55e79701a78b07305ff
Maria Luiza Abrami <156939263+MaluAbrami@users.noreply.github.com>
2024-09-26 22:20:42 -0300

    Aula 2

---
+++ Aula2/EstruturasDeDecisao/EstruturasDeDecisao/Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o primeiro número: ");
+        string num1String = Console.ReadLine();
+        int num1 = Convert.ToInt32(num1String);
+
+        Console.WriteLine("Digite o segundo número: ");
+        string num2String = Console.ReadLine();
+        int num2 = Convert.ToInt32(num2String);
+
+        if (num1 % num2 == 0)
+        {
+            Console.WriteLine("O primeiro número é múltiplo do segundo.");
+        }
+        else
+        {
+            Console.WriteLine("O primeiro número não é múltiplo do segundo.");
+        }
+
+        Console.WriteLine(num1 % num2 == 0 ? "O primeiro número é múltiplo do segundo." : "O primeiro número não é múltiplo do segundo."); //Outra forma de representar
+    }
+}
