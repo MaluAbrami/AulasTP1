4ac1632abb0eccf023db7d9f7f36c5c79479747a
Maria Luiza Abrami <156939263+MaluAbrami@users.noreply.github.com>
2024-11-11 17:30:01 -0300

    tentativa

--- ExercicioAlunos/Aluno.cs
+++ ExercicioAlunos/Aluno.cs
@@ -3,3 +3,83 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+namespace ExercicioAlunos 
+{ 
+    internal class Aluno[]
+    {
+        private int ra;
+        private string nome;
+        private double[] nota;
+        private double media;
+        private bool situacao;
+
+        public int Ra
+        {
+            get { return ra; }
+            set { ra = value; }
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public double[] Nota
+        {
+            get { return nota; }
+            set { nota = value; }
+        }
+
+        public Aluno(int ra, string nome, double[] nota) 
+        {
+            this.ra = ra;
+            this.nome = nome;
+            this.nota = nota;
+            this.media = calcularMedia(nota);
+        }
+
+        public double calcularMedia(double[] nota)
+        {
+            double soma = 0;
+
+            for(int i = 0; i < nota.Lenght; i++)
+            {
+                switch(nota[i])
+                {
+                    case 1://peso 1
+                    case 2://peso 1
+                    case 3://peso 1
+                    {
+                        soma += nota[i]; 
+                        break;
+                    }
+                    case 4://peso 7
+                    {
+                        soma += (nota[i] * 7);
+                        break;
+                    }
+                }
+            }
+
+            return soma/10;
+        }
+
+        public void definirSituacao(double media)
+        {
+            if(media < 4) 
+            {
+                this.situacao = false;
+            }
+            else if(media >= 4 && media < 6)
+            {
+                this.situacao = undefined;
+            }
+            else
+            {
+                this.situacao = true;
+            }
+        }
+    }
+}
--- ExercicioAlunos/Program.cs
+++ ExercicioAlunos/Program.cs
@@ -10,7 +10,29 @@ namespace ExercicioAlunos
     {
         static void Main(string[] args)
         {
+            Aluno aluno1 = new Aluno(1, "Malu", {1, 1, 2, 1});
+            
+            Console.WriteLine(aluno1.ra);
+            Console.WriteLine(aluno1.nome);
 
+            for(int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine($"Nota {i} = {aluno1.nota[i]}");
+            }
+
+            Console.WriteLine($"Media de {aluno1.nome} é {aluno1.media}");
+
+            switch(op)
+            {
+                case 1:
+                {
+                    //CADASTRAR ALUNO
+                }
+                case 2:
+                {
+                    //EXIBIR APROVADOS, RECUPERAÇÃO E REPROVADOS
+                }
+            }
         }
     }
 }
